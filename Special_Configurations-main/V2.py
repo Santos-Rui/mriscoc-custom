06ad96b6bc7795f5be9f44ae9536b097c4ca8288
Rui Santos <61985674+Santos-Rui@users.noreply.github.com>
2023-01-17 15:52:29 +0000

    Base version

---
+++ Special_Configurations-main/V2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# ------------------------------------------------------------------------------
+# Ender3V2 Configurations generator script for the Professional Firmware
+# Author: Miguel A. Risco Castillo
+# URL: https://github.com/mriscoc/Marlin_Configurations
+# ------------------------------------------------------------------------------
+import CreateConfigs
+
+CreateConfigs.Generate('Ender3V2-422-MM', ['Ender3V2','422','MM'])
+CreateConfigs.Generate('Ender3V2-422-BLT', ['Ender3V2','422','BLT'])
+CreateConfigs.Generate('Ender3V2-423-MM', ['Ender3V2','423','MM'])
+CreateConfigs.Generate('Ender3V2-423-BLT', ['Ender3V2','423','BLT'])
+CreateConfigs.Generate('Ender3V2-427-MM', ['Ender3V2','427','MM'])
+CreateConfigs.Generate('Ender3V2-427-BLT', ['Ender3V2','427','BLT'])
+CreateConfigs.Generate('Ender3V2-422-BLTUBL', ['Ender3V2','422','BLT','UBL'])
+CreateConfigs.Generate('Ender3V2-423-BLTUBL', ['Ender3V2','423','BLT','UBL'])
+CreateConfigs.Generate('Ender3V2-427-BLTUBL', ['Ender3V2','427','BLT','UBL'])
+CreateConfigs.Generate('Ender3V2-SKRME3V3-MM', ['Ender3V2','SKRME3V3','MM'])
+CreateConfigs.Generate('Ender3V2-SKRME3V3-BLT', ['Ender3V2','SKRME3V3','BLT'])
+CreateConfigs.Generate('Ender3V2-SKRME3V3-BLTUBL', ['Ender3V2','SKRME3V3','BLT','UBL'])
